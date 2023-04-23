50e9012c517b2d66bd5c33103052bbba3db69194
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-04-22 19:27:36 -0700

    wrote in the prompt questions and writeFile function

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const {Circle, Square, Triangle} = require("./lib/shapes");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { Circle, Square, Triangle } = require("./lib/shapes");
+
+const questions = [
+    {
+    type: "list",
+    message: "What shape wouod you like to choose?",
+    name: "shape",
+    choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+    type: "input",
+    name: "text",
+    message: "TEXT: Enter up to (3) Characters:",
+    },
+    {
+    type: "list",
+    message: "What shape wouod you like to choose?",
+    name: "color",
+    choices: ["Red", "Green", "Blue", "Yellow", "Orange", "Purple", "Pink"],
+    }
+];
+
+function writeFile(fileName, data) {
+    console.log('Writing data to file'); 
+fs.writeFile(fileName, data, function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('Congratulations! You have created an SVG Logo!'); 
+});
+}
 
